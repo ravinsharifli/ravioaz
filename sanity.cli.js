4af6c9b3d0b36e991e9d7a833b95a870586f605a
ravinsharifli <ravinsharifli@gmail.com>
2026-04-09 16:10:49 +0400

    sanity cli config guncellendi

--- sanity.cli.js
+++ sanity.cli.js
@@ -1,8 +1,11 @@
-import { defineCliConfig } from 'sanity/cli'
+import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
     projectId: 'w7scii42',
     dataset: 'production'
-  }
+  },
+  deployment: {
+    appId: 'ap59gvg3davxukwp1le9d4ay',
+  },
 })
