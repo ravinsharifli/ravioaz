209d4c36fb1b3bbdf02461da297ad8a3107b0eda
ravinsharifli <ravinsharifli@gmail.com>
2026-06-14 22:30:21 +0400

    fix: preconnect attrs + Meta Pixel ID

--- public/meta-pixel.js
+++ public/meta-pixel.js
@@ -6,5 +6,5 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '26646673358301164');
+fbq('init', '266466733358301164');
 fbq('track', 'PageView');
